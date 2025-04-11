8e28efb60cacd0dd9bc4df6cd91b62d98d404864
Red Gogh <2968640488@qq.com>
2025-04-11 22:38:27 +0800

    使用 vertex * color 作为三角形颜色

--- shaders/glsl/simple_fragment.glsl
+++ shaders/glsl/simple_fragment.glsl
@@ -6,5 +6,5 @@ layout(location = 0) out vec4 out_color;
 
 void main()
 {
-    out_color = vec4(color, 1.0f); 
+    out_color = vec4(color, 1.0f);
 }
--- shaders/glsl/simple_vertex.glsl
+++ shaders/glsl/simple_vertex.glsl
@@ -3,12 +3,12 @@
 layout(location = 0) in vec3 vertex;
 layout(location = 1) in vec3 color;
 
-layout(location = 0) out vec3 out_color; 
-        
+layout(location = 0) out vec3 out_color;
+
 void main()
 {
     gl_Position = vec4(vertex, 1.0f);
 
-    out_color = color;
+    out_color = color * vertex;
     
 }
